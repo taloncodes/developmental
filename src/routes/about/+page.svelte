1f07b5cdc3a1e1137e82a6d1aebd307a29192a31
tal <talonpouliot@gmail.com>
2025-03-09 12:06:32 +0000

    styling and layout about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,48 +1,65 @@
 <script>
     import '../../app.css';
-    import '../../app.scss'
+    import '../../app.scss';
+    import wave from '$lib/icons/wave_icon.png';
+    import qMark from '$lib/icons/question_icon.png';
+    import tools from '$lib/icons/tools_icon.png';
 </script>
 
 <section>
 
     <div class="w-full p-5 px-[10%] darker xl:px-20 flex xl:text-xl justify-center text-center mb-6 text-accent-one">
-        <h1>About <span>.</span><span>developmen</span><span>./</span><span>tal</span></h1>
+        <h1 class="text-3xl">Inside <span class="text-accent-two">.</span><span class="text-primary">developmen</span><span class="text-accent-two">./</span><span class="text-accent-one">tal</span></h1>
     </div>
 
-    <div class="about xl:h-[396px] xl:max-w-[300px] sharp-corners w-full flex mb-10 xl:mb-0 flex-col justify-start">
-        <h2 class="border-b-2 pb-3 mb-3 top-0 text-accent-one"> Who am I? </h2>
-        <ul class="flex h-full flex-col justify-evenly">
-            Hi, I'm Talon, the owner of Developmental. I have a diverse background in creative digital media, having worked professionally as a developer, computer science teacher, and marketing executive. I love using technology as a creative expression. It's what gives me meaning and makes my work feel less like "work".
+   
+
+    <div class=" p-[5%]">
+
+    <div class="about sharp-corners w-full flex mb-10 flex-col justify-start">
+            <h2 class="text-xl border-b-2 pb-3 mb-3 top-0 text-accent-one inline"> Welcome!</h2>
+
+        <div class="flex flex-col md:flex-row">
+
+        <img src={wave} class="m-auto about-icon w-[75px] h-[75px] md:h-[100px] md:w-[100px] lg:w-[125px] lg:h-[125px]" alt="wave icon">
+        <ul class="flex h-auto flex-col justify-evenly">
+            Hi, I'm Talon, the owner of Developmental. I have a diverse background in creative digital media, having worked professionally as a developer, a computer science teacher, and a marketing executive. I love using technology as a creative expression and helping others bring their visions to life.
         </ul>
-    </div>
-
-    <div class="about xl:h-[396px] xl:max-w-[300px] sharp-corners w-full flex mb-10 xl:mb-0 flex-col justify-start">
-        <h2 class="border-b-2 pb-3 mb-3 top-0 text-accent-one"> What we do </h2>
-        <ul class="flex h-full flex-col justify-evenly">
-            At Developmental, we specialise in crafting modern, functional websites tailored specifically to you. Our goal is to highlight your individuality and help you stand our from your competitors, creating a digital rpesence that's not only memorable but also authentic.
-            Whe use the latest technologies and best practices to ensure your website looks amazing, performs flawlessly, and is optimised for speed and efficiency.
-        </ul>
-    </div>
-
-    <div class="about xl:h-[396px] xl:max-w-[300px] sharp-corners w-full flex mb-10 xl:mb-0 flex-col justify-start">
-        <h2 class="border-b-2 pb-3 mb-3 top-0 text-accent-one"> Why choose us? </h2>
-        <ul class="flex h-full flex-col justify-evenly">
-            Unlike many competitors who rely on cookie-cutter templates, we build fully bespoke websites that balance performance and design. This means you can have as much (or as little) input in the design process as you like. The result? A site that's unique to your business and one that runs faster that a template-based site, without all the built-in bloat that slows things down.
-        </ul>
-    </div>
-
-    <div class="w-full p-5 px-[10%] darker xl:px-20 flex xl:text-xl justify-center text-center mb-6 text-accent-one">
-        Our approach
-    </div>
-
-    <div class="about_container p-10 sm:p-2 flex justify-center w-screen">
-        <p class="w-9/10 md:w-1/2 sharp-corners p-5">
         
-        </p>
+
+    </div>
     </div>
 
-    <div class="w-full p-5 px-[10%] darker xl:px-20 flex xl:text-xl justify-center text-center mb-6 text-accent-one">
-        <h2>Our Approach</h2>
+    
+
+    <div class="about sharp-corners w-full flex mb-10 flex-col justify-start">
+        <h2 class="text-xl border-b-2 pb-3 mb-3 top-0 text-accent-one"> What do we do? </h2>
+
+        <div class="flex flex-col-reverse md:flex-row">
+        
+        <ul class="flex flex-col justify-evenly h-auto">
+            At Developmental, we specialise in crafting modern, functional websites tailored specifically to you. Our goal is to highlight your individuality and help you stand our from your competitors, creating a digital presence that's not only memorable but also authentic.
+            We use the latest technologies and best practices to ensure your website looks amazing, performs flawlessly, and is optimised for speed and efficiency.
+        </ul>
+        <img src={qMark} class="m-auto about-icon w-[75px] h-[75px] md:h-[100px] md:w-[100px] lg:w-[125px] lg:h-[125px]" alt="question mark icon">
+    </div>
+    </div>
+
+    <div class="about sharp-corners w-full flex mb-10 flex-col justify-start">
+        
+        <h2 class="text-xl border-b-2 pb-3 mb-3 top-0 text-accent-one"> Why choose us? </h2>
+        <div class="flex flex-col md:flex-row">
+        <img src={tools} class="m-auto about-icon w-[75px] h-[75px] md:h-[100px] md:w-[100px] lg:w-[125px] lg:h-[125px]" alt="tools icon">
+        <ul class="flex flex-col justify-evenly h-auto">
+            Unlike many competitors who rely on generic templates, we build fully bespoke websites that balance performance and design. This means you can have as much (or as little) input in the design process as you like. The result? A site that's unique to your business and one that runs faster that a template-based site, without all the built-in bloat that slows things down.
+        </ul>
+    </div>
+    </div>
+
+    </div>
+
+    <div class="w-full p-5 px-[10%] border-accent-two border-t-3 darker xl:px-20 flex xl:text-xl justify-center text-center mb-6 text-accent-one">
+        <h1 class="text-3xl">Our Approach</h1>
     </div>
 
     <div class="w-full p-5 px-[10%] darker xl:px-20 flex xl:text-xl justify-center text-center mb-6 text-accent-one">
@@ -74,8 +91,13 @@
         display: flex;
         flex-shrink: 1;
         min-width: 150px;
-        max-height: 370px;
     }
+
+    .about-icon{
+        max-width: 150px;
+        max-height: 150px;
+    }
+
 
 
 </style>
