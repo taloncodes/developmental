bb840c14b7aaa11e4cdc2fcf9b844793321e7fbc
tal <talonpouliot@gmail.com>
2026-01-10 21:38:40 +0000

    svelte:head > head where appropriate

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,10 +16,13 @@
 	import { onMount } from 'svelte';
 
 	afterNavigate((nav) => {
-		gtag('config', 'G-5CRM59K9MX', {
-		page_path: nav.to?.url.pathname
-		});
+  if (typeof window.gtag === 'function') {
+    window.gtag('event', 'page_view', {
+      page_path: nav.to?.url.pathname
+    });
+  	}
 	});
+
 
 	onMount(() => {
 	window.requestAnimationFrame(() => {
@@ -33,46 +36,9 @@
 </script>
 
 <svelte:head>
-<title>{pageMeta[activePath].title}</title>
-<meta name="description" content={pageMeta[activePath].desc}>
-<link rel="canonical" href={`https://developmental.pro${activePath}`} />
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-5CRM59K9MX"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-5CRM59K9MX');
-
-  let fontLink;
-
-  function handleFontLoad() {
-    fontLink.rel = 'stylesheet';
-  }
-</script>
-
-
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
-<link
-  bind:this={fontLink}
-  rel="preload"
-  as="style"
-  href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
-  onload={handleFontLoad}
-/>
-
-<noscript>
-  <link
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
-  />
-</noscript>
-
-
-
+  <title>{pageMeta[activePath].title}</title>
+  <meta name="description" content={pageMeta[activePath].desc} />
+  <link rel="canonical" href={`https://developmental.pro${activePath}`} />
 </svelte:head>
 
 
